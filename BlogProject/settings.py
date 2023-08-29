c45ca56a74decba732a5794a6507d99a05a74d81
narendra25 <anrsmile96@gmail.com>
2023-08-29 18:21:40 +0530

    added

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ic0ql0$)=8(f26&#*qa8!cghc1rsdwrh7wwl$j#^n72t)j8n@n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
